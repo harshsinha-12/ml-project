c25d85d4aaf186d94d1fe152f44d43b2c8c039fd
Harsh Sinha <sinha.harshsep@gmail.com>
2025-06-08 12:18:22 +0530

    Add Setup / Structure

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Reads a requirements file and returns a list of dependencies.
+    """
+    requirements = []
+    with open(file_path, "r") as file:
+        requirements = file.readlines()
+        [req.replace("\n", "") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name="ml-project",
+    version="0.0.1",
+    author="Harsh Sinha",
+    author_email="sinha.harshsep@gmail.com",
+    description="A machine learning project template",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/harshsinha12/ml-project",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
